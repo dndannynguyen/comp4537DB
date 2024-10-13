c8d4a58daea1d68b51c7574463ff2a21b1551096
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-10-13 14:01:59 -0700

    create html css for client side

--- app.js
+++ app.js
@@ -3,17 +3,7 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
-    fs.readFile("./public/insert.html", (err, html) => {
-      if (err) {
-        throw err;
-      }
-      
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(html);
-      res.end();
-    });
-  } else if (req.url == "/query") {
-    fs.readFile("./public/query.html", (err, html) => {
+    fs.readFile("./public/index.html", (err, html) => {
       if (err) {
         throw err;
       }
